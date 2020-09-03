0fbd72de066392de50313d1cb8957249eb2ad85f
sudhir <sudhir0015@gmail.com>
2020-09-03 23:11:33 +0200

    New demo file

---
+++ src/test/java/com/w2a/testcases/DemoTest.java
@@ -0,0 +1,12 @@
+package com.w2a.testcases;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	
+	@Test
+	public void testDemo() {
+		System.out.println("This is my git demo test");
+	}
+
+}
